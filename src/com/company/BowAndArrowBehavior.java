76eff900ee78a731cad0c5ae1ac9cb49260f9fd2
Ilyas2805 <58884526+Ilyas2805@users.noreply.github.com>
2021-09-29 08:58:47 +0600

    Delete BowAndArrowBehavior.java

--- src/com/company/BowAndArrowBehavior.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class BowAndArrowBehavior implements WeaponBehavior{
-    @Override
-    public void useWeapon() {
-        System.out.println("I am using bow and arrow to fight");
-    }
-}
