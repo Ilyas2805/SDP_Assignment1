c621d52c2c0191b888400fed7c975e570f76c888
Ilyas2805 <58884526+Ilyas2805@users.noreply.github.com>
2021-09-29 08:59:02 +0600

    Delete KnifeBehavior.java

--- src/com/company/KnifeBehavior.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class KnifeBehavior implements WeaponBehavior{
-    @Override
-    public void useWeapon() {
-        System.out.println("I am using knife to fight");
-    }
-}
