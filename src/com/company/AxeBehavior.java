8c2dc3a487d9c153ff482cc4b2fd017a2d587a8c
Ilyas2805 <58884526+Ilyas2805@users.noreply.github.com>
2021-09-29 08:58:38 +0600

    Delete AxeBehavior.java

--- src/com/company/AxeBehavior.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class AxeBehavior implements WeaponBehavior{
-    @Override
-    public void useWeapon() {
-        System.out.println("I am using axe to fight");
-    }
-}
